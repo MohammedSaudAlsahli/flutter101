250b743487395c9bb5db25fa66be3480a724065a
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-07-25 03:00:00 +0300

    test

--- helloWorld.dart
+++ helloWorld.dart
@@ -2,7 +2,7 @@ void main() {
   print('hello world');
   var test = "this is a test";
   test = "this is new test"; // it will change
-  const test2 = "this is will never change";
-  test2 = "can change this?"; // becouse of const it will never change
+  // const test2 = "this is will never change";
+  // test2 = "can change this?"; // becouse of const it will never change
   print(test);
 }
