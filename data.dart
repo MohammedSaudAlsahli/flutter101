a4646a2c41ea9bce2529575b1d03a721febbfd3a
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-07-30 09:50:33 +0300

    testing

---
+++ data.dart
@@ -0,0 +1,39 @@
+final List myData = [
+  {
+    "name": "Mohammed",
+    "age": 6,
+    "secretIdentity": "Dan",
+    "powers": ["Radiation resistance"]
+  },
+  {
+    "name": "Seed",
+    "age": 8,
+    "secretIdentity": "Jane Wilson",
+    "powers": [
+      "Million tonne punch",
+      "Damage resistance",
+    ]
+  },
+  {
+    "name": "Ahmed",
+    "age": 23,
+    "secretIdentity": "Wilson",
+    "powers": [
+      "Million tonne punch",
+      "Damage resistance",
+      "Superhuman reflexes"
+    ]
+  },
+  {
+    "name": "Slman",
+    "age": 29,
+    "secretIdentity": "Jane freek",
+    "powers": ["Damage resistance", "Superhuman reflexes"]
+  },
+  {
+    "name": "Madame Uppercut",
+    "age": 44,
+    "secretIdentity": "Wilson dison",
+    "powers": ["Million tonne punch", "Superhuman reflexes"]
+  }
+];
